d378b035d02eb0e2bd7662f150f74c12eaeee95b
Andrey Beryukhov <beryukhov-andrey@yandex.ru>
2018-10-06 14:03:09 +0300

    iOS project & Framework Dependency

---
+++ native/KotlinIOS/KotlinIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KotlinIOS
+//
+//  Created by Андрей Берюхов on 06.10.2018.
+//  Copyright © 2018 Beryukhov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
