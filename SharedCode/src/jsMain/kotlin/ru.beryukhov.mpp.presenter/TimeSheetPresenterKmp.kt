481acd35b228d91239f642b4482edf57f8aa61f1
Andrey Beryukhov <beryukhov-andrey@yandex.ru>
2019-08-24 17:02:32 +0300

    add js module

---
+++ SharedCode/src/jsMain/kotlin/ru.beryukhov.mpp.presenter/TimeSheetPresenterKmp.kt
@@ -0,0 +1,91 @@
+package ru.beryukhov.mpp.presenter
+
+import com.soywiz.klock.DateTime
+import ru.beryukhov.mpp.domain.DateModel
+import ru.beryukhov.mpp.domain.TimeSheetInteractor
+import ru.beryukhov.mpp.domain.TimeSheetInteractorImpl
+import ru.beryukhov.mpp.domain.TimeSheetRepository
+import ru.beryukhov.mpp.presenter.TimeSheetPresenter.Companion.getStartDate
+import ru.beryukhov.mpp.view.TimeSheetView
+
+import kotlin.browser.*
+import kotlinx.html.*
+import kotlinx.html.dom.*
+
+
+/**
+ * Created by Andrey Beryukhov
+ * Expected that all the multithreading logic is solved by js code
+ * Presenter method should be called from background thread
+ * View methods implementation should return to main (UI) thread
+ */
+
+fun main() {
+    println("Hello JavaScript!")
+    printHello()
+}
+
+fun printHello() {
+    window.onload = {
+        document.body!!.append.div {
+            span {
+                +"Hello"
+            }
+        }
+    }
+}
+
+actual class TimeSheetPresenterKmp actual constructor(timeSheetView: TimeSheetView, timeSheetRepository: TimeSheetRepository) {
+    val timeSheetInteractor: TimeSheetInteractor
+    val timeSheetView: TimeSheetView
+
+    init {
+        timeSheetInteractor = TimeSheetInteractorImpl(timeSheetRepository)
+        this.timeSheetView = timeSheetView
+    }
+
+    /**
+     * Should be called from background thread
+     */
+    actual fun onCreateView() {
+        timeSheetView.showProgress()
+        val dates = timeSheetInteractor.getDatesListBlocking(getStartDate())
+        timeSheetView.clear()
+        timeSheetView.addAll(dates)
+        timeSheetView.hideProgress()
+    }
+
+    /**
+     * Should be called from background thread
+     */
+    actual fun onFixStart() {
+        timeSheetView.showProgress()
+        timeSheetInteractor.addStartTimeBlocking(DateTime.now())
+        val dates = timeSheetInteractor.getDatesListBlocking(getStartDate())
+        timeSheetView.clear()
+        timeSheetView.addAll(dates)
+        timeSheetView.hideProgress()
+
+    }
+
+
+    /**
+     * Should be called from background thread
+     */
+    actual fun onFixEnd() {
+        timeSheetView.showProgress()
+
+
+        timeSheetInteractor.addEndTimeBlocking(DateTime.now())
+        val dates = timeSheetInteractor.getDatesListBlocking(getStartDate())
+        timeSheetView.clear()
+        timeSheetView.addAll(dates)
+        timeSheetView.hideProgress()
+
+    }
+
+
+    actual fun onItemClick(item: DateModel) {
+    }
+
+}
