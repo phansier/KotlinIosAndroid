cca21e4b1bc4d39b879e0a8b6455eb72e1531576
Andrey Beryukhov <beryukhov-andrey@yandex.ru>
2019-08-28 19:24:42 +0300

    add endpoints (post doesn't work)

--- backend/src/main/kotlin/ru/beryukhov/mpp/SampleJvm.kt
+++ backend/src/main/kotlin/ru/beryukhov/mpp/SampleJvm.kt
@@ -1,11 +1,24 @@
 package ru.beryukhov.mpp
 
 import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
 import io.ktor.html.respondHtml
+import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.Parameters
+import io.ktor.http.content.MultiPartData
+import io.ktor.http.content.TextContent
 import io.ktor.http.content.resource
 import io.ktor.http.content.static
+import io.ktor.request.receive
+import io.ktor.request.receiveMultipart
+import io.ktor.request.receiveParameters
+import io.ktor.response.respond
 import io.ktor.routing.get
+import io.ktor.routing.post
 import io.ktor.routing.routing
+import io.ktor.serialization.serialization
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import kotlinx.html.body
@@ -15,8 +28,15 @@ import kotlinx.html.id
 import kotlinx.html.script
 import kotlinx.html.title
 
+data class PostSnippet(val snippet: Text) {
+    data class Text(val text: String)
+}
+
 fun main() {
     embeddedServer(Netty, port = 8080, host = "127.0.0.1") {
+        install(ContentNegotiation) {
+            serialization()
+        }
         routing {
             get("/") {
                 call.respondHtml {
@@ -33,6 +53,18 @@ fun main() {
                     }
                 }
             }
+            get("/api") {
+                call.respond(TextContent("{}", ContentType.Application.Json))
+            }
+            post("/fixStart"){
+                val post = call.receive<PostSnippet>()
+                println(post)
+                call.respond(HttpStatusCode.OK)
+            }
+            post("/fixEnd"){
+                call.respond(HttpStatusCode.OK)
+            }
+
             static("/static") {
                 resource("KotlinIosAndroid-SharedCode.js")
             }
