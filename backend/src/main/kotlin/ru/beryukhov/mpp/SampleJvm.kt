b99233e88da32f83e6ff81cbeafb17dd86dbdc7f
Andrey Beryukhov <beryukhov-andrey@yandex.ru>
2019-08-28 19:24:42 +0300

    add test endpoints

--- backend/src/main/kotlin/ru/beryukhov/mpp/SampleJvm.kt
+++ backend/src/main/kotlin/ru/beryukhov/mpp/SampleJvm.kt
@@ -3,39 +3,33 @@ package ru.beryukhov.mpp
 import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.ContentNegotiation
+import io.ktor.gson.GsonConverter
 import io.ktor.html.respondHtml
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
-import io.ktor.http.Parameters
-import io.ktor.http.content.MultiPartData
 import io.ktor.http.content.TextContent
 import io.ktor.http.content.resource
 import io.ktor.http.content.static
 import io.ktor.request.receive
-import io.ktor.request.receiveMultipart
-import io.ktor.request.receiveParameters
 import io.ktor.response.respond
 import io.ktor.routing.get
 import io.ktor.routing.post
 import io.ktor.routing.routing
-import io.ktor.serialization.serialization
+
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import kotlinx.html.body
-import kotlinx.html.div
-import kotlinx.html.head
-import kotlinx.html.id
-import kotlinx.html.script
-import kotlinx.html.title
+import kotlinx.html.*
 
-data class PostSnippet(val snippet: Text) {
-    data class Text(val text: String)
+data class PostSnippet(val snippet: Text?) {
+    data class Text(val text: String?)
 }
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "127.0.0.1") {
+
         install(ContentNegotiation) {
-            serialization()
+            //register(ContentType.Application.Json, SerializationConverter())
+            register(ContentType.Application.Json, GsonConverter())
         }
         routing {
             get("/") {
@@ -56,10 +50,17 @@ fun main() {
             get("/api") {
                 call.respond(TextContent("{}", ContentType.Application.Json))
             }
+            post("/"){
+                val map = call.receive<Map<*, *>>()
+                val text = map.entries.joinToString { "${it.key}=${it.value}" }
+                call.respond(text)
+            }
             post("/fixStart"){
+                println("call=$call")
                 val post = call.receive<PostSnippet>()
                 println(post)
-                call.respond(HttpStatusCode.OK)
+                call.respond(post)
+                //call.respond(HttpStatusCode.OK)
             }
             post("/fixEnd"){
                 call.respond(HttpStatusCode.OK)
