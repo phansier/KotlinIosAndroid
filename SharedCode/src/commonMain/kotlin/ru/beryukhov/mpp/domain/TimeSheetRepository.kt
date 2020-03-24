7cdfff1d2ebca7bbd3e17e0cbe617057407c1aa2
Andrey Beryukhov <beryukhov-andrey@yandex.ru>
2020-03-24 21:41:33 +0300

    Start convert gradle to kotlin

--- SharedCode/src/commonMain/kotlin/ru/beryukhov/mpp/domain/TimeSheetRepository.kt
+++ SharedCode/src/commonMain/kotlin/ru/beryukhov/mpp/domain/TimeSheetRepository.kt
@@ -3,6 +3,9 @@ package ru.beryukhov.mpp.domain
 import com.soywiz.klock.DateTime
 import io.ktor.client.HttpClient
 import io.ktor.client.call.call
+import io.ktor.client.request.request
+import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.response
 import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode.Companion.OK
 import io.ktor.http.Url
@@ -17,11 +20,11 @@ open class BaseTimeSheetRepositoryImpl : TimeSheetRepository {
         val client = HttpClient()
         val address = Url("https://beryukhov.ru/")
 
-        val call = client.call(address) {
+        val call = client.request<HttpResponse>(address) {
             method = HttpMethod.Get
         }
         client.close()
-        if (call.response.status == OK) {
+        if (call.status == OK) {
             return listOf(
                     DateTimeRecord(DateTime.createAdjusted(2019, 9, 22, 9), true),
                     DateTimeRecord(DateTime.createAdjusted(2019, 9, 22, 17, 30), false)
